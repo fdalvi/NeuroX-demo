6fabe052b414ec21d9de5d817c562343c0a43bf8
Fahim Imaduddin Dalvi <faimaduddin@qf.org.qa>
2018-10-30 10:40:30 -0400

    Added DB populator

--- src/server/populate_db.py
+++ src/server/populate_db.py
@@ -59,28 +59,36 @@ project_1_ranking_1.store = {
 					'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-es-2.pt_10ktrain.tok.pt',
 					'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-es-3.pt_10ktrain.tok.pt'
 				],
-				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/cross_correlations/en-es-10k-activations.json'
+				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/cross_correlations/en-es-10k-activations.json',
+				'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Model-2-3-Cross-Model-Ranking.txt',
+				'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Model-2-3-Cross-Model-Ranking-Reverse.txt'
 			}
 project_1_ranking_1.save()
 project_1_ranking_2.store = {
 				'activations': [
 					'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-es-1.pt_10ktrain.tok.pt',
 				],
-				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_POS/en-es-1.pt_10ktrain.tok/top_neurons.json'
+				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_POS/en-es-1.pt_10ktrain.tok/top_neurons.json',
+				'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Part-of-Speech-Ranking.txt',
+				'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Part-of-Speech-Ranking-Reverse.txt'
 			}
 project_1_ranking_2.save()
 project_1_ranking_3.store = {
 				'activations': [
 					'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-es-1.pt_10ktrain.tok.pt',
 				],
-				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/univariate_POS/en-es-1.pt_10ktrain.tok.json'
+				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/univariate_POS/en-es-1.pt_10ktrain.tok.json',
+				'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Part-of-Speech-Univariate-Feature-Selection.txt',
+				'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Part-of-Speech-Univariate-Feature-Selection-Reverse.txt'
 			}
 project_1_ranking_3.save()
 project_1_ranking_4.store = {
 				'activations': [
 					'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-es-1.pt_10ktrain.tok.pt',
 				],
-				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_SEM/en-es-1.pt_14ktrain.tok/top_neurons.json'
+				'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_SEM/en-es-1.pt_14ktrain.tok/top_neurons.json',
+				'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Semantic-Tags-Ranking.txt',
+				'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-es/Semantic-Tags-Ranking-Reverse.txt'
 			}
 project_1_ranking_4.save()
 
@@ -137,7 +145,9 @@ project_2_ranking_1.store = {
 						'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-ar-2.pt_10ktrain.tok.pt',
 						'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-ar-3.pt_10ktrain.tok.pt'
 					],
-					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/cross_correlations/en-ar-10k-activations.json'
+					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/cross_correlations/en-ar-10k-activations.json',
+					'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Model-2-3-Cross-Model-Ranking.txt',
+					'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Model-2-3-Cross-Model-Ranking-Reverse.txt'
 				}
 project_2_ranking_1.save()
 
@@ -145,7 +155,9 @@ project_2_ranking_2.store = {
 					'activations': [
 						'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-ar-1.pt_10ktrain.tok.pt',
 					],
-					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_POS/en-ar-1.pt_10ktrain.tok/top_neurons.json'
+					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_POS/en-ar-1.pt_10ktrain.tok/top_neurons.json',
+					'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Part-of-Speech-Ranking.txt',
+					'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Part-of-Speech-Ranking-Reverse.txt'
 				}
 project_2_ranking_2.save()
 
@@ -153,7 +165,9 @@ project_2_ranking_3.store = {
 					'activations': [
 						'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-ar-1.pt_10ktrain.tok.pt',
 					],
-					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/univariate_POS/en-ar-1.pt_10ktrain.tok.json'
+					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/univariate_POS/en-ar-1.pt_10ktrain.tok.json',
+					'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Part-of-Speech-Univariate-Feature-Selection.txt',
+					'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Part-of-Speech-Univariate-Feature-Selection-Reverse.txt'
 				}
 project_2_ranking_3.save()
 
@@ -161,7 +175,9 @@ project_2_ranking_4.store =  {
 					'activations': [
 						'/data/sls/qcri/mt/work/NeuroDissection/test_data/decoded_activations/en-ar-1.pt_10ktrain.tok.pt',
 					],
-					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_SEM/en-ar-1.pt_14ktrain.tok/top_neurons.json'
+					'ranking': '/data/sls/qcri/mt/work/NeuroDissection/test_data/linguistic_correlations_SEM/en-ar-1.pt_14ktrain.tok/top_neurons.json',
+					'ablation_forward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Semantic-Tags-Ranking.txt',
+					'ablation_backward': '/data/sls/qcri/mt/work/NeuroDissection/test_data/ablation_results/en-ar/Semantic-Tags-Ranking-Reverse.txt'
 				}
 project_2_ranking_4.save()
 print("Database:")
